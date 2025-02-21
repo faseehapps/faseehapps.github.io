81b74374f9650e85f455e8e54279540171b0a1aa
Faseeh <faseeh1080@gmail.com>
2025-02-21 08:20:36 +0530

    review submit refresh

--- script.js
+++ script.js
@@ -23,23 +23,7 @@ toggleButton.addEventListener("click", () => {
 });
 
 
-document.getElementById('review-form').addEventListener('submit', async function(event) {
-    event.preventDefault(); // Prevent the default form submission
-
-    const formData = new FormData(this);
-    const response = await fetch('https://faseeh1080.pythonanywhere.com/submit-review', {
-        method: 'POST',
-        body: formData
-    });
-
-    if (response.ok) {
-        alert('Review submitted successfully!');
-    } else {
-        alert('Failed to submit review.');
-    }
-});
-
-
+// Fetch reviews
 async function fetchReviews() {
     let reviewList = document.getElementById('review-list');
 
@@ -60,3 +44,23 @@ async function fetchReviews() {
 }
 
 fetchReviews();
+
+
+// Post reviews
+document.getElementById('review-form').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const formData = new FormData(this);
+    const response = await fetch('https://faseeh1080.pythonanywhere.com/submit-review', {
+        method: 'POST',
+        body: formData
+    });
+
+    if (response.ok) {
+        let reviewFormDiv = document.getElementById('review-form-div');
+        reviewFormDiv.innerHTML = '<p>Thanks for submitting your review.</p>';
+        fetchReviews();
+    } else {
+        alert('Failed to submit the review.');
+    }
+});
