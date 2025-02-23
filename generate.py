94457e343ad10e65b5fead9c867906966143a6a9
Faseeh <faseeh1080@gmail.com>
2025-02-23 11:50:01 +0530

    file to store path

--- generate.py
+++ generate.py
@@ -66,8 +66,12 @@ def generator():
     print("Generaing HTML...")
 
     content = ""
-    for filename in os.listdir("src"):
-        content += download_item(os.path.join("src", filename))
+
+    with open("src_files.txt", 'r', encoding='utf-8') as file:
+        src_files = file.read().split()
+
+    for src_filename in src_files:
+        content += download_item(os.path.join("src", src_filename + '.yaml'))
 
     with open("index.html", 'w', encoding='utf-8') as html_file:
         html_file.write(HTML_TEMPLATE.replace("{content}", add_identation(content, 2)))
