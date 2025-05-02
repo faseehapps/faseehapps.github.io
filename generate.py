6dc76e1ce2eb856bb78eea023f19d427f1809e98
Faseeh <faseeh1080@gmail.com>
2025-05-02 18:06:08 +0530

    refresh

--- generate.py
+++ generate.py
@@ -5,14 +5,17 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import time
 
+
 def generator(logs: bool = True) -> None:
     # To print messages
     def log(message: str) -> None:
         if logs:
             print(message)
-    
-    with open('template.html', 'r', encoding='utf-8') as file:
-        HTML_TEMPLATE = file.read() # Replace {content} substring with the actual content
+
+    with open("template.html", "r", encoding="utf-8") as file:
+        HTML_TEMPLATE = (
+            file.read()
+        )  # Replace {content} substring with the actual content
 
     # Generate HTML
     log("Generaing HTML...")
@@ -20,39 +23,45 @@ def generator(logs: bool = True) -> None:
     content = ""
     src_files = []
 
-    with open("src_files.txt", 'r', encoding='utf-8') as file:
+    with open("src_files.txt", "r", encoding="utf-8") as file:
         src_files = file.read().split()
 
     for src_filename in src_files:
-        content += download_item(os.path.join("src", src_filename + '.yaml'))
+        content += download_item(os.path.join("src", src_filename + ".yaml"))
 
-    with open("index.html", 'w', encoding='utf-8') as html_file:
+    with open("index.html", "w", encoding="utf-8") as html_file:
         html_file.write(HTML_TEMPLATE.replace("{content}", add_identation(content, 2)))
 
     # Merge CSS
     log("Merging CSS files into styles.css...")
-    with open("styles.css", 'w', encoding='utf-8') as css_file:
+    with open("styles.css", "w", encoding="utf-8") as css_file:
         css_file.write(merge_css_files("stylesheets"))
 
     log("Process complete!")
 
+
 class OnFileChange(FileSystemEventHandler):
     def on_modified(self, event):
-        if event.is_directory: # Ignore directory changes, only track file changes
+        if event.is_directory:  # Ignore directory changes, only track file changes
             return
-        
+
         filename = os.path.basename(event.src_path)
-        if filename in ("index.html", "styles.css"): # Ignore generated files
+        if filename in ("index.html", "styles.css"):  # Ignore generated files
             return
-        
+
         generator(logs=False)
-        print("Refreshed.")
+        global refresh
+        refresh += 1
+        print(f"\033[KRefresh: {refresh}", end="\r", flush=True)
+
 
 # Start
 
-if input("Activate debug mode? (y/N): ").strip().lower() in ('y', 'yes'):
-    print('Debug mode actiavted. Press Ctrl + C to exit.')
-    generator(logs=False) # Run generator initially
+refresh = 0
+
+if input("Activate debug mode? (y/N): ").strip().lower() in ("y", "yes"):
+    print("Debug mode actiavted. Press Ctrl + C to exit.")
+    generator(logs=False)  # Run generator initially
 
     path_to_watch = "."
     event_handler = OnFileChange()
@@ -64,10 +73,10 @@ if input("Activate debug mode? (y/N): ").strip().lower() in ('y', 'yes'):
     try:
         while True:
             time.sleep(0.25)
-    except KeyboardInterrupt: # Ctrl + C
+    except KeyboardInterrupt:  # Ctrl + C
         observer.stop()
-    
-    observer.join() # Wait for the observer thread to finish
+
+    observer.join()  # Wait for the observer thread to finish
     print("Program stopped.")
 else:
     generator()
